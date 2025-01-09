6aa941f0b3f5900e4506bce63934710937fc57a7
Alejandro Lavado <alejandro.lavado@grupobios.cl>
2025-01-09 10:20:26 -0300

    setup inicial

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -1,45 +1,46 @@
 import "./Calendar.css";
-import { useAppointments } from "./hooks";
+import { useWeeklyAppointments } from "./hooks";
 
 const Calendar = () => {
-  const { data, isLoading } = useAppointments({
-    week: 2,
-    year: 2025,
-  });
+  const { data, isLoading } = useWeeklyAppointments(new Date());
 
   if (isLoading) {
     return "Cargando...";
   }
 
+  console.log({ data });
+
   return (
     <div className="Calendar__container">
-      <div className="Calendar__days">
-        <div className="Calendar__day">
-          <div>Lunes</div>
-        </div>
-        <div className="Calendar__day">
-          <div>Martes</div>
-        </div>
-        <div className="Calendar__day">
-          <div>Miércoles</div>
-        </div>
-        <div className="Calendar__day">
-          <div>Jueves</div>
-        </div>
-        <div className="Calendar__day">
-          <div>Viernes</div>
-        </div>
-        <div className="Calendar__day">
-          <div>Sábado</div>
-        </div>
-        <div className="Calendar__day">
-          <div>Domingo</div>
-        </div>
-      </div>
+      <div className="Calendar__main"></div>
       <button className="Calendar__previous_week_button">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={24}
+          height={24}
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M15.41 16.58L10.83 12l4.58-4.59L14 6l-6 6l6 6z"
+          ></path>
+        </svg>{" "}
         Semana anterior
       </button>
-      <button className="Calendar__next_week_button">Semana siguiente</button>
+      <button className="Calendar__next_week_button">
+        Semana siguiente{" "}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M8.59 16.58L13.17 12L8.59 7.41L10 6l6 6l-6 6z"
+          />
+        </svg>
+      </button>
     </div>
   );
 };
--- src/hooks.ts
+++ src/hooks.ts
@@ -1,19 +1,19 @@
+import { addDays, format, startOfWeek } from "date-fns";
 import { useEffect, useState } from "react";
+import { es } from "date-fns/locale";
+import { fakerES_MX as faker } from "@faker-js/faker";
 
 type AppointmentBlock = {
   available: boolean;
   patientName?: string;
 };
 
-export const useAppointments = ({
-  week,
-  year,
-}: {
-  week: number;
-  year: number;
-}): {
+export const useWeeklyAppointments = (
+  date: Date
+): {
   data: {
     dayLabel: string;
+    monthLabel: string;
     blocks: {
       "8:00 - 9:00": AppointmentBlock;
       "9:00 - 10:00": AppointmentBlock;
@@ -41,8 +41,39 @@ export const useAppointments = ({
     return () => clearTimeout(timeout);
   }, []);
 
+  let data = [];
+  let currentDate = startOfWeek(date, { weekStartsOn: 1 });
+  for (let i = 0; i < 7; i++) {
+    data.push({
+      dayLabel: format(currentDate, "iiii d", { locale: es }),
+      monthLabel: format(currentDate, "MMMM yy", { locale: es }),
+      blocks: {
+        "8:00 - 9:00": generateBlock(),
+        "9:00 - 10:00": generateBlock(),
+        "10:00 - 11:00": generateBlock(),
+        "11:00 - 12:00": generateBlock(),
+        "12:00 - 13:00": generateBlock(),
+        "13:00 - 14:00": generateBlock(),
+        "14:00 - 15:00": generateBlock(),
+        "15:00 - 16:00": generateBlock(),
+        "16:00 - 17:00": generateBlock(),
+        "17:00 - 18:00": generateBlock(),
+        "18:00 - 19:00": generateBlock(),
+      },
+    });
+    currentDate = addDays(currentDate, 1);
+  }
+
   return {
     isLoading,
-    data: [],
+    data,
   };
 };
+
+const generateBlock = () =>
+  Math.random() > 0.4
+    ? {
+        available: false,
+        patientName: faker.person.firstName() + " " + faker.person.lastName(),
+      }
+    : { available: true };
